decb3da82a73c21764ea0c84752d0b32bbf8798d
OVNZxx <272586@unifio.edu.br>
2026-09-15 19:10:44 +0000

    teste3

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/repositorios/PedidoRepositorio.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/repositorios/PedidoRepositorio.java
@@ -3,6 +3,6 @@ package br.edu.unifio.ecommerce.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.unifio.ecommerce.entidades.Pedido;
 
-public interface PedidoRepositorio extends JpaRepository<Pedido, Short> {
+public interface PedidoRepositorio extends JpaRepository<Pedido, Integer> {
 
 }
