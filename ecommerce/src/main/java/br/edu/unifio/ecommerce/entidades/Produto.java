dcca8e3b710a9f5adf49811377418b672edb7d55
OVNZxx <272586@unifio.edu.br>
2026-09-15 19:08:26 +0000

    teste2

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Produto.java
@@ -7,10 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "tb_produto")
 @Getter
 @Setter
 public class Produto {
--- ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/ProdutoRepositorioTests.java
@@ -1,5 +1,28 @@
 package br.edu.unifio.ecommerce.repositorios;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.edu.unifio.ecommerce.entidades.Produto;
+
+@SpringBootTest
 public class ProdutoRepositorioTests {
 
+    @Autowired
+    private ProdutoRepositorio produtoRepositorio;
+
+    @Test
+    public void deveBuscarUmProdutoPorId() {
+        Produto produto = produtoRepositorio.findById(1).orElseThrow();
+
+        assertNotNull(produto);
+        assertEquals("Smartphone", produto.getNome());
+        assertEquals(new BigDecimal("2500.00"), produto.getPreco());
+    }
 }
