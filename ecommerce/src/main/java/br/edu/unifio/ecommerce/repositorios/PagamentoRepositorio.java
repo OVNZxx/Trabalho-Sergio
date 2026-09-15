1f664c3e6191794991d234aa3b0564c9fb0d73f1
OVNZxx <272586@unifio.edu.br>
2026-09-15 19:15:31 +0000

    teste 5

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/repositorios/PagamentoRepositorio.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/repositorios/PagamentoRepositorio.java
@@ -3,6 +3,6 @@ package br.edu.unifio.ecommerce.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.unifio.ecommerce.entidades.Pagamento;
 
-public interface PagamentoRepositorio extends JpaRepository<Pagamento, Short> {
+public interface PagamentoRepositorio extends JpaRepository<Pagamento, Integer> {
 
 }
--- ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/PagamentoRepositorioTests.java
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/PagamentoRepositorioTests.java
@@ -1,5 +1,29 @@
 package br.edu.unifio.ecommerce.repositorios;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.edu.unifio.ecommerce.entidades.Pagamento;
+
+@SpringBootTest
 public class PagamentoRepositorioTests {
 
+    @Autowired
+    private PagamentoRepositorio pagamentoRepositorio;
+
+    @Test
+    public void deveBuscarUmPagamentoPorId() {
+        Pagamento pagamento = pagamentoRepositorio.findById(1).orElseThrow();
+
+        assertNotNull(pagamento);
+        assertEquals(new BigDecimal("2500.00"), pagamento.getValor());
+        assertEquals("PENDENTE", pagamento.getStatus());
+        assertEquals("PIX", pagamento.getTipo());
+    }
 }
